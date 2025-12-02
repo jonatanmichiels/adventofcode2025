40fbb683ca92c83bfc1b3f2411f8d6a35b4eca1b
Jonatan Michiels <michielsjonatan@gmail.com>
2025-12-02 20:41:14 +0100

    Day two 🎄

---
+++ day_2/second.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+inputs = File.read(File.join(__dir__, 'data.txt'), chomp: true).split(',')
+
+pattern = /^(\d+)\1{1,}$/
+matches = []
+
+inputs.each do |input|
+  start_num, end_num = input.split('-').map(&:to_i)
+
+  (start_num..end_num).each do |num|
+    matches << num if num.to_s =~ pattern
+  end
+end
+
+puts matches.map(&:to_i).sum
